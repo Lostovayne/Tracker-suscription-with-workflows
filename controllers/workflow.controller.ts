e24ad6158a7a2ae1092a8c63e83ebe2bc2cb0f3b
LostoVayne <moises.barrera.dev@gmail.com>
2025-02-26 03:06:42 -0300

    feat: añadir lógica de recordatorios de renovación con dayjs

--- controllers/workflow.controller.ts
+++ controllers/workflow.controller.ts
@@ -1,8 +1,12 @@
 // import { createRequire } from "module";
 // const require = createRequire(import.meta.url);
 import type { WorkflowContext } from "@upstash/workflow";
+import dayjs from "dayjs";
+
 import { serve } from "@upstash/workflow/express";
 import Subscription from "../models/subscription.model";
+
+const REMINDERS = [7, 5, 2, 1];
 
 export const sendReminders = serve(async (context: WorkflowContext<unknown>) => {
   const { subscriptionId } = context.requestPayload as {
@@ -13,6 +17,11 @@ export const sendReminders = serve(async (context: WorkflowContext<unknown>) => 
   if (!subscription || subscription.status !== "active") return;
 
   // TODO:RenewalDate logic here
+  const renewalDate = dayjs(subscription.renewalDate);
+  if (renewalDate.isBefore(dayjs())) {
+    console.log(`Renewal date is in the past for subscription ${subscriptionId}. Stopping workflow.`);
+    return;
+  }
 });
 
 const fetchSubscription = async (context: WorkflowContext, subscriptionId: string) => {
